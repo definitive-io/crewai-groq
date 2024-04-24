5cf141222b63e4a2a27ae9b7b54caf1b49931b24
dloman118 <99347459+dloman118@users.noreply.github.com>
2024-04-24 17:05:23 -0400

    remove llama2 from model dropdown

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def main():
     st.sidebar.title('Customization')
     model = st.sidebar.selectbox(
         'Choose a model',
-        ['mixtral-8x7b-32768', 'llama2-70b-4096']
+        ['llama3-8b-8192', 'mixtral-8x7b-32768', 'gemma-7b-it']
     )
 
     llm = ChatGroq(
